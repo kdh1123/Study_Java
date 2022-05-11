2dc18fabd7b19e66ebd03272f78e1a1a316fb79d
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-05-11 09:55:57 +0900

    Create : FileStudy
    2022-05-11

---
+++ src/kr/hs/dgsw/java/K0511/FileStudy.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.java.K0511;
+
+import java.io.File;
+
+public class FileStudy {
+    public void studyFileData(){
+        File file1 = new File("C:/Users/DGSW/Documents/GitHub/Study_Java/sample.txt");
+        File file2 = new File("C:/Users/DGSW/Documents/GitHub/Study_Java/void.txt");
+        File file3 = new File("C:/Users/DGSW/Documents/GitHub/Study_Java");
+
+
+    }
+}
