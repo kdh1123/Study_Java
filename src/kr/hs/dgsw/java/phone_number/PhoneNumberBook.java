e9f517486ff6d2bc318b30c1f535ea3d09931e9a
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-06-14 20:29:03 +0900

    Create PhoneNumberBook.java
    2022-06-14

---
+++ src/kr/hs/dgsw/java/phone_number/PhoneNumberBook.java
@@ -0,0 +1,28 @@
+package kr.hs.dgsw.java.phone_number;
+
+import kr.hs.dgsw.java.K0504.FileWriter;
+
+import java.util.Scanner;
+
+public class PhoneNumberBook {
+    public Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        PhoneNumberBook book = new PhoneNumberBook();
+    }
+    public void savePhoneNum(){
+        FileWriter writer = new FileWriter("D:/PhoneNumbers.txt");
+
+    }
+    public void searchUseName(){
+
+    }
+    public void searchUseNum(){
+
+    }
+    public void deletePhoneNum(){
+
+    }
+    public void updatePhoneNum(){
+
+    }
+}
